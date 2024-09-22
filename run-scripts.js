3ba4a6936065191e76068efc162291fe6f2465f7
Sergey Egorov <thehaha@yandex.ru>
2024-09-23 00:39:45 +0300

    add run-scripts

---
+++ run-scripts.js
@@ -0,0 +1,4 @@
+require('dotenv').config();
+
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const CHAT_ID = process.env.CHAT_ID;
--- sketch.js
+++ sketch.js
@@ -1,6 +1,3 @@
-const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-const CHAT_ID = process.env.CHAT_ID;
-
 let video;
 let button;
 let img;
