1798486c2214c56f27d4821d892c2cfb587e544f
Sergey Egorov <thehaha@yandex.ru>
2024-09-23 00:46:38 +0300

    chat-id log

--- run-scripts.js
+++ run-scripts.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
 
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-const CHAT_ID = '-1002425906440';
+const CHAT_ID = process.env.CHAT_ID;
+
+console.log('chat id is:', CHAT_ID);
