3e87e2b10023cf33b6e0f201ac8520e283cf8d3b
Sergey Egorov <thehaha@yandex.ru>
2024-09-23 11:04:49 +0300

    add cors

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 // server.js
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Включение CORS
+app.use(cors({
+  origin: 'https://hayabuzo.github.io', // Разрешить запросы только с этого домена
+}));
 
 // Пример маршрута для получения скрытых переменных
 app.get('/api/secrets', (req, res) => {
   // Здесь будем использовать переменные из окружения
-  const secretValue = process.env.TELEGRAM_BOT_TOKEN;
+  const secretValue = process.env.MY_SECRET;
   res.json({ secret: secretValue });
 });
 
