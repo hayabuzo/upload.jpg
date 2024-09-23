cf74806b0e3f4e477f172a8a241da41e9a5ac570
Sergey Egorov <thehaha@yandex.ru>
2024-09-23 21:38:55 +0300

    upd: gui

--- sketch.js
+++ sketch.js
@@ -3,17 +3,22 @@ let button;
 let img;
 let fit;
 
+
 let CID = '-1002425906440';
 let TBT = undefined;
 
-let state = 'capture';
+let state = 'init';
+let camState = "🙄🔎📸";
+let camStateSize = 1;
 
 const imgSize = [480,640];
 const screenScale = 1.0;
 
 const tr = {
   y: 640,
-  ySet: 0,
+  ySet: 640,
+  s: 1,
+  sSet: 1,
 }
 
 function setup() {
@@ -33,12 +38,14 @@ function setup() {
       height: {max: imgSize[1]}, 
       facingMode: "environment" 
     }
-  });
+  },
+  { flipped: false },
+  () => {
+    state = 'capture';
+    fit = video.width>video.height*0.75 ? img.height/video.height : img.width/video.width;
+  }
+);
 	video.hide(); 
-
-  // button = createButton('📸');
-  // button.id('snapshotButton');
-  // button.mousePressed(takeSnapshot);
 
 	img = createGraphics(imgSize[0],imgSize[1]);
   img.imageMode(CENTER);
@@ -50,29 +57,62 @@ function setup() {
 
 }
 
+function drawImage() {
+  imageMode(CENTER)
+  image(img,0.5*width,0.5*height+tr.y,width*tr.s,height*tr.s);
+}
+
+function drawBorder() {
+  push();
+  noFill();
+  stroke(255,(1-tr.s)*10*255);
+  strokeWeight(8);
+  rectMode(CENTER);
+  rect(width/2, height/2, width*tr.s, height*tr.s);
+  pop();
+}
+
+function drawCamState() {
+  push();
+    textSize(window.innerHeight*0.1*camStateSize);
+    text(camState, width/2, height/2);
+  pop();
+}
+
 function draw() {
 
   background(10);
+  drawCamState();
 
   tr.y += (tr.ySet-tr.y)/15;
+  tr.s += (tr.sSet-tr.s)/15;
 
   switch (state) {
+
+    case 'init':
+      break;
+      
     case 'capture':
-      fit = video.width>video.height*0.75 ? img.height/video.height : img.width/video.width;
       img.image(video,imgSize[0]/2,imgSize[1]/2,video.width*fit,video.height*fit); 
       tr.ySet = 0;
-      image(img,0,tr.y,width,height);
+      tr.sSet = 1;
+
+      drawImage();
+      drawBorder()
       text("📸", width/2, height*0.95);
       if (mouseIsPressed && mouseY>height*0.8) { 
         mouseIsPressed = false; 
         state = 'review'
       }
       break;
+
     case 'review':
-      image(img,0,tr.y,width,height);
-      text("🔁", width*0.25, height*0.95);
-      text("✉️", width*0.75, height*0.95);
-      if (mouseIsPressed && mouseY>height*0.8) { 
+      drawImage();
+      drawBorder();
+      text("🔁", width*0.25, height*0.85);
+      text("✉️", width*0.75, height*0.85);
+      tr.sSet = 0.9;
+      if (mouseIsPressed && mouseY>height*0.6) { 
         mouseIsPressed = false; 
         if (mouseX<width*0.5) {
           state = 'capture';
@@ -82,13 +122,14 @@ function draw() {
         }
       }
       break;
+
     case 'sending':
       tr.ySet = height;
       push();
-        textSize(window.innerHeight*0.2);
-        text("📩", width*0.5, height*0.5);
+        camState = "💌"
+        camStateSize = 2;
       pop();
-      image(img,0,tr.y,width,height);
+      drawImage();
       text("🔁", width*0.5, height*0.95);
       if (mouseIsPressed) { 
         mouseIsPressed = false; 
@@ -99,6 +140,7 @@ function draw() {
         }
       }
       break;
+
   }
 
 
